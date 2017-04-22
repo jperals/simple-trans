643b4853e8a8bed0679fe26043877f3635a39a7d
Joan Perals <joan.perals@ixds.com>
2017-04-22 23:22:28 +0200

    Refine feedback to user

--- src/components/TranslationCell.vue
+++ src/components/TranslationCell.vue
@@ -14,42 +14,74 @@
               v-model="msgId">
     </textarea>
     <div class="status">
-      <icon name="circle-o-notch" class="spinner" v-if="unresolved"></icon>
-      <icon name="check" class="checkmark" v-if="saved"></icon>
+      <span v-if="unresolved" class="saving">
+        <span class="message">Saving…</span>
+        <icon name="circle-o-notch" class="icon spinner"></icon>
+      </span>
+      <span v-if="saved" class="saved">
+        <span class="message">Saved</span>
+        <icon name="check" class="icon"></icon>
+      </span>
+      <span v-if="error" class="error" v-bind:title="error">
+        <span class="message">Error</span>
+        <icon name="exclamation-triangle" class="icon"></icon>
+      </span>
     </div>
   </td>
 </template>
 <style scoped>
   td {
     position: relative;
+    padding: 0;
   }
   textarea {
-    border: 0 none;
+    border: 1px solid transparent;
+    background: transparent;
     resize: none;
     width: calc(100% - 2 * 12px);
     display: block;
     outline: none;
     padding: 12px;
+    /*margin-left: -1px;*/
+  }
+  textarea:focus {
+    border-color: rgba(63, 207, 63, .75);
+    width: calc(100% - 2 * 12px - 1px);
   }
   .status {
     position: absolute;
     bottom: 0;
     right: 0;
     padding: 0 6px;
+    font-size: 14px;
   }
-  .spinner {
+  .status .saving {
     color: rgba(127, 127, 127, .25);
-    animation-name: infinite-spinning;
-    animation-duration: 1s;
-    animation-iteration-count: infinite;
-    animation-timing-function: linear;
   }
-  .checkmark {
+  .status .saved {
     color: rgba(63, 207, 63, .75);
     animation-name: fade-out;
     animation-duration: 3s;
     animation-delay: 2s;
     animation-fill-mode: forwards;
+  }
+  .status .error {
+    color: rgba(207, 63, 63, .75);
+  }
+  .status .message {
+    line-height: 14px;
+    vertical-align: bottom;
+  }
+  .status .icon {
+    vertical-align: middle;
+    height: 14px;
+    width: 14px;
+  }
+  .spinner {
+    animation-name: infinite-spinning;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
   }
   @keyframes fade-out {
     from {
@@ -72,6 +104,7 @@
   import Vue from 'vue'
   import 'vue-awesome/icons/check'
   import 'vue-awesome/icons/circle-o-notch'
+  import 'vue-awesome/icons/exclamation-triangle'
   import Icon from 'vue-awesome/components/Icon.vue'
   Vue.component('icon', Icon)
   export default {
@@ -82,6 +115,7 @@
     ],
     data: function () {
       return {
+        error: false,
         saved: false,
         unresolved: 0
       }
@@ -121,6 +155,10 @@
             .then(function (response) {
               this.unresolved -= 1
               this.saved = this.unresolved < 1
+            }.bind(this))
+            .catch(function (error) {
+              this.unresolved -= 1
+              this.error = error
             }.bind(this))
         }
       },
